c4948cb14849bcf44171020bfabdfa2ae349e672
voice-tracer <sound@pc-studio.dk>
2014-12-05 12:58:02 +0100

    commit GetStudents

--- server/dataLayer.js
+++ server/dataLayer.js
@@ -6,22 +6,39 @@ function getStudents(callback) {
     studentsModel.find({}, function (err, data) {
         if (err) result(err);
         else {
-            result(null, data);
+            callback (null, data);
         }
 
     });
 };
+
+function getStudent(userName,callback) {
+
+    studentsModel.find({userName:userName})
+        // .populate('classId')  // Problemer: dokument classes i mongodb  virker ikke når vi vil have dokument classes med!
+        .exec(function (err, data) {
+        if (err)
+            callback(err);
+        else {
+            callback (null, data);
+        }
+
+    });
+};
+
 function getClasses(callback)  {
     classesModel.find({}, function (err, data) {
         if (err)
-            result(err);
+            callback(err);
         else {
-            result(null, data)
+            callback (null, data)
         }
     });
 
 };
 module.exports = {
     getStudents: getStudents,
-    getClasses: getClasses
+    getClasses: getClasses,
+    getStudent: getStudent      //exporterer
+
 };
